55e59f561baafd647ede696360b532f0da891d24
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-07-14 14:45:56 +0530

    Update product.routes.js

--- Express/modules/product/routes/product.routes.js
+++ Express/modules/product/routes/product.routes.js
@@ -5,9 +5,9 @@ const ProductController = require('../controller/product.controller');
 const productController = new ProductController();
 
 router.get('/all', productController.getAllProducts);
-router.post('/create', productController.createProduct);
+router.post('/', productController.createProduct);
 router.get('/search', productController.searchProduct);
-router.get('/top-rated/:limit', productController.getTopRatedProducts);
+router.get('/top/:limit', productController.getTopRatedProducts);
 router.get('/:id', productController.getProduct);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
